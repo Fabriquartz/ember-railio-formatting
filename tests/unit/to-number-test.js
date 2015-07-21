728a603ee5d03713372a283dd868d08dd29fcd93
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-07-21 09:22:24 +0200

    write tests for 100% code coverage

--- tests/unit/to-number-test.js
+++ tests/unit/to-number-test.js
@@ -3,6 +3,10 @@ import { toNumber } from 'ember-railio-to-number';
 
 module('toNumber');
 
+test('number 10.10 to number', function(assert) {
+  assert.equal(toNumber(10.10), 10.10);
+});
+
 test('10,10 to number', function(assert) {
   assert.equal(toNumber('10,10'), 10.10);
 });
@@ -21,4 +25,13 @@ test('1.000,10 to number', function(assert) {
 
 test('1 000,10 to number', function(assert) {
   assert.equal(toNumber('1 000,10'), 1000.10);
+});
+
+test('date to number', function(assert) {
+  const date = new Date(2015, 0, 1, 8, 8);
+  assert.equal(toNumber(date), 1420096080000);
+});
+
+test('none number to number', function(assert) {
+  assert.equal(toNumber(true), null);
 });
