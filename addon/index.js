c34b04363fac50c468d9af407b96acb1b39d567b
Chantal Broeren <chantal.broeren@fabriquartz.com>
2015-08-10 15:40:13 +0200

    add format-date convertion and helper

--- addon/index.js
+++ addon/index.js
@@ -1,3 +1,17 @@
+import $ from 'jquery';
+
+const THOUSANDS_REGEX = /(\d+)(\d{3})/;
+
+function addThousandSeperators(integer) {
+  let string = integer.toString();
+
+  while (integer >= 1000 && THOUSANDS_REGEX.test(string)) {
+    string = string.replace(THOUSANDS_REGEX, '$1.$2');
+  }
+
+  return string;
+}
+
 export function toNumber(value) {
   if (typeof value === 'number') { return value; }
 
@@ -23,4 +37,38 @@ export function toNumber(value) {
   }
 
   return null;
+}
+
+export function formatNumber(value, { decimals } = {}) {
+  const originalValue = value;
+  value = toNumber(value);
+
+  if (typeof value !== 'number' || isNaN(value)) { return originalValue; }
+
+  const integerValue = Math.floor(value);
+  const decimalValue = value - integerValue;
+
+  const integerStringValue = addThousandSeperators(integerValue);
+
+  if (decimalValue > 0 || decimals > 0) {
+    let decimalStringValue = decimalValue.toFixed(decimals).slice(2);
+    return `${integerStringValue},${decimalStringValue}`;
+  } else {
+    return integerStringValue;
+  }
+}
+
+export function formatDate(value, options) {
+  if (!(value instanceof Date)) { return value; }
+  if (options === null || typeof object !== 'object') {
+    options = {};
+  }
+  options = $.extend({
+    year:   'numeric',
+    month:  '2-digit',
+    day:    '2-digit',
+    hour:   'numeric',
+    minute: 'numeric'
+  }, options);
+  return value.toLocaleString('nl-NL', options);
 }
