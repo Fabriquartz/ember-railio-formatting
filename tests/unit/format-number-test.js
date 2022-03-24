a3bf9cd4dc9bbf20997ee28757d7c8565abe728e
Michael <michael.wenting@fabriquartz.com>
2022-03-24 10:08:50 +0100

    Upgrade Ember to 4.2.0 (#49)

--- tests/unit/format-number-test.js
+++ tests/unit/format-number-test.js
@@ -3,11 +3,11 @@ import { formatNumber } from 'ember-railio-formatting';
 
 module('Unit | formatNumber', function () {
   test('formatNumber returns a stringified number', function (assert) {
-    assert.equal(formatNumber(10), '10');
+    assert.strictEqual(formatNumber(10), '10');
   });
 
   test('formatNumber returns a replacement if undefined', function (assert) {
-    assert.equal(
+    assert.strictEqual(
       formatNumber(undefined, { replacement: '-' }),
       '-',
       'Shows replacement when value is undefinded'
@@ -15,7 +15,7 @@ module('Unit | formatNumber', function () {
   });
 
   test('formatNumber returns a replacement if null', function (assert) {
-    assert.equal(
+    assert.strictEqual(
       formatNumber(null, { replacement: '-' }),
       '-',
       'Shows replacement when value is null'
@@ -23,7 +23,7 @@ module('Unit | formatNumber', function () {
   });
 
   test('formatNumber returns a replacement if empty string', function (assert) {
-    assert.equal(
+    assert.strictEqual(
       formatNumber('', { replacement: '-' }),
       '-',
       'Shows replacement when value is an empty string'
@@ -31,7 +31,7 @@ module('Unit | formatNumber', function () {
   });
 
   test('formatNumber returns a replacement if empty array', function (assert) {
-    assert.equal(
+    assert.strictEqual(
       formatNumber([], { replacement: '-' }),
       '-',
       'Shows replacement when value is an empty array'
@@ -39,47 +39,53 @@ module('Unit | formatNumber', function () {
   });
 
   test('formatNumber returns 0 if value is 0', function (assert) {
-    assert.equal(formatNumber(0), 0, 'Returns 0 when value is 0');
+    assert.strictEqual(formatNumber(0), '0', 'Returns 0 when value is 0');
   });
 
   test('formatNumber by default formats with same decimals', function (assert) {
-    assert.equal(formatNumber(10.123), '10,123');
+    assert.strictEqual(formatNumber(10.123), '10,123');
   });
 
   test('formatNumber formats a negative value', function (assert) {
-    assert.equal(formatNumber(-3.5), '-3,5');
+    assert.strictEqual(formatNumber(-3.5), '-3,5');
   });
 
   test('formatNumber formats a negative value between -1 and 0', function (assert) {
-    assert.equal(formatNumber(-0.6), '-0,6');
+    assert.strictEqual(formatNumber(-0.6), '-0,6');
   });
 
   test('formatNumber formats by decimals', function (assert) {
     assert.expect(101);
 
-    assert.equal(formatNumber(10.121, { decimals: 2 }), '10,12');
-    assert.equal(formatNumber(10.2, { decimals: 2 }), '10,20');
+    assert.strictEqual(formatNumber(10.121, { decimals: 2 }), '10,12');
+    assert.strictEqual(formatNumber(10.2, { decimals: 2 }), '10,20');
 
     for (let i = 0; i < 94; i++) {
       let number = i;
 
-      assert.equal(
+      assert.strictEqual(
         formatNumber(number / 100, { decimals: 1 }),
         `0,${Math.round(number / 10)}`,
         'slices decimals'
       );
     }
 
-    assert.equal(formatNumber(0.95, { decimals: 1 }), '1,0');
-    assert.equal(formatNumber(0.96, { decimals: 1 }), '1,0');
-    assert.equal(formatNumber(0.97, { decimals: 1 }), '1,0');
-    assert.equal(formatNumber(0.98, { decimals: 1 }), '1,0');
-    assert.equal(formatNumber(0.99, { decimals: 1 }), '1,0');
+    assert.strictEqual(formatNumber(0.95, { decimals: 1 }), '1,0');
+    assert.strictEqual(formatNumber(0.96, { decimals: 1 }), '1,0');
+    assert.strictEqual(formatNumber(0.97, { decimals: 1 }), '1,0');
+    assert.strictEqual(formatNumber(0.98, { decimals: 1 }), '1,0');
+    assert.strictEqual(formatNumber(0.99, { decimals: 1 }), '1,0');
   });
 
   test('formatNumber formats a large number', function (assert) {
-    assert.equal(formatNumber(1600000.987), '1 600 000,987');
-    assert.equal(formatNumber(1600000.987, { decimals: 2 }), '1 600 000,99');
-    assert.equal(formatNumber(1600000.987, { decimals: 4 }), '1 600 000,9870');
+    assert.strictEqual(formatNumber(1600000.987), '1 600 000,987');
+    assert.strictEqual(
+      formatNumber(1600000.987, { decimals: 2 }),
+      '1 600 000,99'
+    );
+    assert.strictEqual(
+      formatNumber(1600000.987, { decimals: 4 }),
+      '1 600 000,9870'
+    );
   });
 });
--- tests/unit/to-number-test.js
+++ tests/unit/to-number-test.js
@@ -3,35 +3,35 @@ import { toNumber } from 'ember-railio-formatting';
 
 module('Unit | toNumber', function () {
   test('number 10.10 to number', function (assert) {
-    assert.equal(toNumber(10.1), 10.1);
+    assert.strictEqual(toNumber(10.1), 10.1);
   });
 
   test('10,10 to number', function (assert) {
-    assert.equal(toNumber('10,10'), 10.1);
+    assert.strictEqual(toNumber('10,10'), 10.1);
   });
 
   test('10.10 to number', function (assert) {
-    assert.equal(toNumber('10.10'), 10.1);
+    assert.strictEqual(toNumber('10.10'), 10.1);
   });
 
   test('1,000.10 to number', function (assert) {
-    assert.equal(toNumber('1,000.10'), 1000.1);
+    assert.strictEqual(toNumber('1,000.10'), 1000.1);
   });
 
   test('1.000,10 to number', function (assert) {
-    assert.equal(toNumber('1.000,10'), 1000.1);
+    assert.strictEqual(toNumber('1.000,10'), 1000.1);
   });
 
   test('1 000,10 to number', function (assert) {
-    assert.equal(toNumber('1 000,10'), 1000.1);
+    assert.strictEqual(toNumber('1 000,10'), 1000.1);
   });
 
   test('date to number', function (assert) {
     let date = new Date(1420096080000);
-    assert.equal(toNumber(date), 1420096080000);
+    assert.strictEqual(toNumber(date), 1420096080000);
   });
 
   test('none number to number', function (assert) {
-    assert.equal(toNumber(true), null);
+    assert.strictEqual(toNumber(true), null);
   });
 });
